d4c987661c8c586e89e32adf6ce9fc7ea1f490b2
DynoW <104089032+DynoW@users.noreply.github.com>
2023-05-24 15:40:35 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-process_name="python"
+process_name="python3"
 
 process_pid=$(pgrep -f "$process_name")
 
 if [ -n "$process_pid" ]; then
   echo "Killing Python process with PID $process_pid"
   kill "$process_pid"
-  nohup python /path/to/script.py > /dev/null &
+  nohup python3 main.py > /dev/null &
 else
   echo "Python process is not running."
-  nohup python /path/to/script.py > /dev/null &
+  nohup python3 main.py > /dev/null &
 fi
