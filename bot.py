30e37886f340d5d163457ad5e6eddf35166d1cf0
DynoW <104089032+DynoW@users.noreply.github.com>
2023-06-06 15:32:13 +0300

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+import time
 import pymongo
 import numpy as np
 import os
@@ -11,15 +12,17 @@ catalog = db["catalog"]
 listaElevi = db["elevi"]
 
 # Env variables --------------------------------------------------------------------------------------------------------
-bot = commands.Bot(command_prefix="$", description="Comenzi pentru DynoW BOT:", intents=discord.Intents.all())
+bot = commands.Bot(command_prefix="$", description="Comenzi pentru DynoW BOT:", help_command = commands.DefaultHelpCommand(no_category = 'Help'), intents=discord.Intents.all())
 
 # Events ---------------------------------------------------------------------------------------------------------------
 @bot.event
 async def on_ready():
-    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="$comenzi | dynow.tk/bot"))
-    print("\x1B[3mStatus: \x1B[0m" + "Bot is online!")
+    await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="$help | dynow.tk/bot"))
+    await bot.add_cog(Fun())
+    await bot.add_cog(Catalog())
+    print("\x1B[36mStatus\x1B[0m: " + "Bot is online!")
     for guild in bot.guilds:
-        print("\x1B[3mServer: \x1B[0m" + guild.name)
+        print("\x1B[95mServer\x1B[0m: " + guild.name)
 
 # Functions ------------------------------------------------------------------------------------------------------------
 def calcMedii():
@@ -34,135 +37,161 @@ def calcMedii():
 calcMedii()
 
 # Commands -------------------------------------------------------------------------------------------------------------
-@bot.command(pass_context=True)
-async def ping(ctx):
-    """Returns a pong!"""
-    await ctx.send("pong")
 
-@bot.command()
-async def zile(ctx, obj: str):
-    current_time = datetime.now()
-    if obj == "scoala":
-        momentspecial = datetime(2023, 6, 15)
-        timpramas = np.busday_count(current_time.date(), momentspecial.date(), holidays=['2023-05-01', '2023-06-01', '2023-06-02', '2023-06-05', '2023-06-16'])
-        await ctx.send("Mai sunt ~" + str(timpramas) + " zile de scoala.")
-    if obj == "vacanta":
-        momentspecial = datetime(2023, 6, 15)
-        timpramas = momentspecial - current_time
-        await ctx.send("Mai sunt ~" + str(timpramas.days) + " zile pana la vacanta.")    
-    if obj == "bac":
-        momentspecial = datetime(2025, 6, 12)
-        timpramas = momentspecial - current_time
-        await ctx.send("Mai sunt ~" + str(timpramas.days) + " zile pana la bac.")
-
-# Commands with embends ------------------------------------------------------------------------------------------------
-@bot.command()
-async def comenzi(ctx):
-    await ctx.send("Command is temoraly disabed!")
+class Fun(commands.Cog):
+    """Comenzi amuzante"""
+    
+    @commands.command()
+    async def ping(self, ctx):
+        """Returns a pong!"""
+        await ctx.send("pong")
+        
+    @commands.command()
+    async def zile(self, ctx, obj: str):
+        """scoala | vacanta | bac"""
+        current_time = datetime.now()
+        if obj == "scoala":
+            momentspecial = datetime(2023, 6, 15)
+            timpramas = np.busday_count(current_time.date(), momentspecial.date(), holidays=['2023-05-01', '2023-06-01', '2023-06-02', '2023-06-05', '2023-06-16'])
+            await ctx.send("Mai sunt " + str(timpramas) + " zile de scoala.")
+        if obj == "vacanta":
+            momentspecial = datetime(2023, 6, 15)
+            timpramas = momentspecial - current_time
+            await ctx.send("Mai sunt " + str(timpramas.days) + " zile pana la vacanta.")    
+        if obj == "bac":
+            momentspecial = datetime(2025, 6, 12)
+            timpramas = momentspecial - current_time
+            await ctx.send("Mai sunt " + str(timpramas.days) + " zile pana la bac.")
 
 # Catalog commands -----------------------------------------------------------------------------------------------------
-@bot.command()
-async def elevi(ctx):
-    mesaj = ""
-    for elev in listaElevi.find():
-        if elev["elevId"]!="":
-            mesaj = mesaj + f"""{elev["$id"]}. {elev["nume"]} - `{elev["elevId"]}`\n"""
-        else:
-            mesaj = mesaj + f"""{elev["$id"]}. {elev["nume"]} - \n"""
-    await ctx.send(mesaj)
-
-@bot.command()
-async def all(ctx):
-    mesaj = ""
-    for medii in mediiElevi:
-        v=0
+class Catalog(commands.Cog):
+    """Comenzi pentru catalog"""
+    
+    @commands.command()
+    async def all(self, ctx):
+        """Vezi toate id-urile"""
+        mesaj = ""
+        for medii in mediiElevi:
+            v=0
+            for elev in listaElevi.find():
+                if elev["elevId"]==medii["elevId"]:
+                    mesaj = mesaj + f"""`{medii["elevId"]}` - {medii["medie"]} - {elev["nume"]}\n"""
+                    v=1
+            if v==0:
+                mesaj = mesaj + f"""`{medii["elevId"]}` - {medii["medie"]}\n"""
+        await ctx.send(mesaj)
+    
+    @commands.command()
+    async def elevi(self, ctx):
+        """Vezi toti elevii"""
+        mesaj = ""
         for elev in listaElevi.find():
-            if elev["elevId"]==medii["elevId"]:
-                mesaj = mesaj + f"""`{medii["elevId"]}` - {medii["medie"]} - {elev["nume"]}\n"""
-                v=1
-        if v==0:
-            mesaj = mesaj + f"""`{medii["elevId"]}` - {medii["medie"]}\n"""
-    await ctx.send(mesaj)
+            if elev["elevId"]!="":
+                mesaj = mesaj + f"""{elev["$id"]}. {elev["nume"]} - `{elev["elevId"]}`\n"""
+            else:
+                mesaj = mesaj + f"""{elev["$id"]}. {elev["nume"]} - \n"""
+        await ctx.send(mesaj)
+        
+    @commands.command()
+    async def top5(self, ctx):
+        """5 cei mai buni din clasa"""
+        mediiMax = [{"elevId": "None", "medie": 0}]*5
+        for media in mediiElevi:
+            if media["medie"] > mediiMax[0]["medie"]:
+                mediiMax[4] = mediiMax[3]
+                mediiMax[3] = mediiMax[2]
+                mediiMax[2] = mediiMax[1]
+                mediiMax[1] = mediiMax[0]
+                mediiMax[0] = media
+            elif media["medie"] > mediiMax[1]["medie"]:
+                mediiMax[4] = mediiMax[3]
+                mediiMax[3] = mediiMax[2]
+                mediiMax[2] = mediiMax[1]
+                mediiMax[1] = media
+            elif media["medie"] > mediiMax[2]["medie"]:
+                mediiMax[4] = mediiMax[3]
+                mediiMax[3] = mediiMax[2]
+                mediiMax[2] = media
+            elif media["medie"] > mediiMax[3]["medie"]:
+                mediiMax[4] = mediiMax[3]
+                mediiMax[3] = media
+            elif media["medie"] > mediiMax[4]["medie"]:
+                mediiMax[4] = media
+        embed = discord.Embed(title="Cei mai buni 5 elevi din clasa:", color=discord.Color.blue())
+        for i in range(0, 5):
+            v=0
+            for elev in listaElevi.find():
+                if mediiMax[i]["elevId"] == elev["elevId"]:
+                    embed.add_field(name=f"Top {i+1}", value=f"""**{mediiMax[i]["medie"]}** - `{mediiMax[i]["elevId"]}` - {elev["nume"]}""", inline=False)
+                    v=1
+            if v==0:
+                embed.add_field(name=f"Top {i+1}", value=f"""**{mediiMax[i]["medie"]}** - `{mediiMax[i]["elevId"]}`""", inline=False)
+        embed.set_footer(text="Pentru ajutor contactati: DynoW#9056")
+        await ctx.send(embed=embed)
 
-@bot.command()
-async def top5(ctx):
-    mediiMax = [{"elevId": "None", "medie": 0}]*5
-    for media in mediiElevi:
-        if media["medie"] > mediiMax[0]["medie"]:
-            mediiMax[4] = mediiMax[3]
-            mediiMax[3] = mediiMax[2]
-            mediiMax[2] = mediiMax[1]
-            mediiMax[1] = mediiMax[0]
-            mediiMax[0] = media
-        elif media["medie"] > mediiMax[1]["medie"]:
-            mediiMax[4] = mediiMax[3]
-            mediiMax[3] = mediiMax[2]
-            mediiMax[2] = mediiMax[1]
-            mediiMax[1] = media
-        elif media["medie"] > mediiMax[2]["medie"]:
-            mediiMax[4] = mediiMax[3]
-            mediiMax[3] = mediiMax[2]
-            mediiMax[2] = media
-        elif media["medie"] > mediiMax[3]["medie"]:
-            mediiMax[4] = mediiMax[3]
-            mediiMax[3] = media
-        elif media["medie"] > mediiMax[4]["medie"]:
-            mediiMax[4] = media
-    embed = discord.Embed(title="Cei mai buni 5 elevi din clasa:",
-                          color=discord.Color.blue())
-    for i in range(0, 5):
-        v=0
-        for elev in listaElevi.find():
-            if mediiMax[i]["elevId"] == elev["elevId"]:
-                embed.add_field(name=f"Top {i+1}", value=f"""**{mediiMax[i]["medie"]}** - `{mediiMax[i]["elevId"]}` - {elev["nume"]}""", inline=False)
-                v=1
-        if v==0:
-            embed.add_field(name=f"Top {i+1}", value=f"""**{mediiMax[i]["medie"]}** - `{mediiMax[i]["elevId"]}`""", inline=False)
-    embed.set_footer(text="Pentru ajutor contactati: DynoW#9056")
-    await ctx.send(embed=embed)
+    @commands.command()
+    async def medi(self, ctx, elevId: str):
+        """[id_elev] - Verifica mediile cuiva"""
+        mesaj = ""
+        for elev in catalog.find():
+            if elev["elevId"] == elevId:
+                for medie in elev["Medii"]:
+                    mesaj = mesaj + f"""*{medie["Nume"]}* - *{str(medie["Nota"])}* - Rang: {medie["Rang"]}\n"""
+        await ctx.send(mesaj)
 
-@bot.command()
-async def note(ctx, elevId: str):
-    mesaj = ""
-    for elev in catalog.find():
-        if elev["elevId"] == elevId:
-            for materi in elev["Materii"]:
-                mesaj = mesaj + f"""*{materi["Nume"]}* - """
-                for nota in materi["Despre"][0]["data"]:
-                    mesaj = mesaj + f"""*{str(round(nota[1]))}*  """
-                mesaj = mesaj + "\n"
-    await ctx.send(mesaj)
+    @commands.command()
+    async def note(self, ctx, elevId: str):
+        """[id_elev] - Verifica notele cuiva"""
+        mesaj = ""
+        for elev in catalog.find():
+            if elev["elevId"] == elevId:
+                for materi in elev["Materii"]:
+                    mesaj = mesaj + f"""*{materi["Nume"]}* - """
+                    for nota in materi["Despre"][0]["data"]:
+                        mesaj = mesaj + f"""*{str(round(nota[1]))}*  """
+                    mesaj = mesaj + "\n"
+        await ctx.send(mesaj)
 
-@bot.command()
-async def medi(ctx, elevId: str):
-    mesaj = ""
-    for elev in catalog.find():
-        if elev["elevId"] == elevId:
-            for medie in elev["Medii"]:
-                mesaj = mesaj + f"""*{medie["Nume"]}* - *{str(medie["Nota"])}* - Rang: {medie["Rang"]}\n"""
-    await ctx.send(mesaj)
-
-@bot.command()
-async def sync(ctx):
-    calcMedii()
-    await ctx.send("Done!")
+    @commands.command()
+    async def sync(self,ctx):
+        """Sinconizare cu baza de date"""
+        calcMedii()
+        await ctx.send("Done!")
     
 # Listening events -----------------------------------------------------------------------------------------------------
 @bot.listen()
 async def on_message(message):
     if "ntza" in message.content.lower():
         await message.channel.send('Dyno BOT V2.1 is here!')
-
-@bot.listen()
-async def on_message(message):
     if "test" in message.content.lower():
         await message.add_reaction("<💩>")
-
-@bot.listen()
-async def on_message(message):
     if "care" in message.content.lower() and message.author.id == 494105470714511360:
         await message.channel.send("pe care")
-
+    if "kill" in message.content.lower():
+        if message.author.id == 455608238335983617:
+            await message.channel.send("I'M DEAD 💀")
+            await bot.change_presence(status=discord.Status.invisible)
+        else:
+            await message.channel.send("Ur not my boss :middle_finger:")
+    if "revive" in message.content.lower():
+        if message.author.id == 455608238335983617:
+            await message.channel.send("I'M ALIVE 🥹")
+            await bot.change_presence(status=discord.Status.online, activity=discord.Game(name="$help | dynow.tk/bot"))
+        else:
+            await message.channel.send("Ur not my boss :middle_finger:")
+    if "shutdown" in message.content.lower():
+        if message.author.id == 455608238335983617:
+            mesaj = await message.channel.send("SHUTTING DOWN")
+            time.sleep(1)
+            await mesaj.edit(content="SHUTTING DOWN.")
+            time.sleep(1)
+            await mesaj.edit(content="SHUTTING DOWN..")
+            time.sleep(1)
+            await mesaj.edit(content="SHUTTING DOWN...")
+            exit(0)
+        else:
+            await message.channel.send("You don't have permission")
+    
 # Token ----------------------------------------------------------------------------------------------------------------
 bot.run(os.environ["TOKEN"])
 
